7a85961f75810d96861e3e5409749c873db1b5b4
henildhir <dhirajlal.henil297@gmail.com>
2025-12-22 02:53:38 +0000

    gdp per capita graph

--- analysis_dataset.py
+++ analysis_dataset.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 gdp = pd.read_csv("World_gdp/new_world_gdp.csv")
 population = pd.read_csv("World_population/new_world_population.csv")
@@ -32,3 +34,26 @@ print(merged.head())
 print(merged.columns)
 
 merged.to_csv("analysis_dataset.csv", index="False")
+
+dataset = pd.read_csv("analysis_dataset.csv")
+
+plt.figure(figsize=(10,6))
+sns.lineplot(data=dataset,x="Year",
+             y="CO2 per capita (Trillions)",
+             hue="Country Name",
+             marker="o",
+             linewidth=2,
+             markersize=5)
+plt.xlabel=("Year")
+plt.ylabel("Co2 emissions per capita (tonnes)")
+plt.title("CO2 emissions per capita (tonnes) over time since 1980")
+plt.grid(True,alpha=0.3)
+
+Paris_agreement=2015
+
+plt.axvline(x=Paris_agreement,color="grey",linestyle="--",linewidth=1.5,alpha=0.7,label=f"{Paris_agreement} Paris Agreement")
+handles,labels = plt.gca().get_legend_handles_labels()
+plt.legend(handles,labels,title="Country/Event",loc="best")
+
+plt.tight_layout()
+plt.show()

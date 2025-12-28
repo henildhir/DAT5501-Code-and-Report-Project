c87f00b4eca7387e8950192b3e99294b9895131c
henildhir <dhirajlal.henil297@gmail.com>
2025-12-28 22:55:23 +0000

    annual-percentage-change

---
+++ Figures/annual_%_change.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset=pd.read_csv("analysis_dataset.csv")
+dataset["Year"]=pd.to_numeric(dataset["Year"],errors="coerce")
+
+countries = ["World"]
+
+subplot = []
+for country in countries:
+    data_country=(dataset[dataset["Country Name"]==country]
+                  .sort_values("Year")
+                  .copy())
+    
+    data_country["CO2_percentage_change"] = (
+        data_country["CO2 per capita (Trillions)"].pct_change()*100
+    )
+
+    data_country["GDP_percentage_change"] = (
+        data_country["GDP per capita"].pct_change()*100
+    )
+
+    subplot.append(data_country)
+
+change=pd.concat(subplot, ignore_index=True)
+
+fig,axes = plt.subplots(2,1,figsize=(10,8), sharex=True)
+
+for country in countries:
+    data_country = change[change["Country Name"]==country]
+    axes[0].plot(data_country["Year"],data_country["CO2_percentage_change"],
+                 marker="o",linewidth=1.5,)
+
+axes[0].axhline(0, color ="black", linewidth=1)
+axes[0].set_ylabel("Annual % change in CO2 per capita")
+axes[0].set_title("Annual % chagnge in CO2 emissions per capita since 1980")
+axes[0].grid(True, alpha=0.3)
+
+for country in countries:
+    data_country = change[change["Country Name"] ==country]
+    axes[1].plot(data_country["Year"],data_country["GDP_percentage_change"],
+                 marker="o",linewidth=1.5)
+
+axes[1].axhline(0,color="black",linewidth=1)
+axes[1].set_xlabel("Year")
+axes[1].set_ylabel("Annual % change in gdp per capita")
+axes[1].set_title("Annual % change in gdp per capita since 1980")
+axes[1].grid(True,alpha=0.3)
+
+
+
+for ax in axes:
+    ax.spines["top"].set_visible(True)
+    ax.spines["right"].set_visible(True)
+
+plt.tight_layout()
+plt.show()

9023f9f155956effc10ee55a29402e9b4043e745
henildhir <dhirajlal.henil297@gmail.com>
2025-12-29 23:56:14 +0000

    colour changes

--- Figures/co2_emissions_gdp_per_capita.py
+++ Figures/co2_emissions_gdp_per_capita.py
@@ -9,7 +9,7 @@ max_year=dataset["Year"].max()
 min_year=max_year-9
 filter=dataset[(dataset["Year"]>=min_year)&(dataset["Year"]<=max_year)]
 
-plt.figure(figsize=(8,6))
+plt.figure(figsize=(10,6))
 
 ax = sns.scatterplot(
     data=filter,
@@ -18,16 +18,22 @@ ax = sns.scatterplot(
     hue="Country Name",
     marker="o",
     alpha=0.4,
+    palette={
+        "United Kingdom":"green",
+        "World":"red",
+        "India":"blue",
+        "Malawi":"orange",
+    },
 )
 
 plt.xlabel("Gdp per capita ($))")
 plt.ylabel("CO2 emissions per capita (tonnes)")
 plt.title("CO2 per capita vs GDP per capita")
-plt.legend(title="Country",loc="best")
+plt.legend(title="Country",loc="lower right")
 
 folder="Graphs"
 os.makedirs(folder,exist_ok=True)
-# plt.savefig(os.path.join(folder,"co2_emissions_gdp_per_capita.png"))
+plt.savefig(os.path.join(folder,"co2_emissions_gdp_per_capita.png"))
 
 ax.spines["top"].set_visible(True)
 ax.spines["right"].set_visible(True)

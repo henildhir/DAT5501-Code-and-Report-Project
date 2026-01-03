8b5066a6267798d0a2eb22107333c7371df959b2
henildhir <dhirajlal.henil297@gmail.com>
2026-01-03 15:44:41 +0000

    axes modified

--- Figures/annual_%_change.py
+++ Figures/annual_%_change.py
@@ -24,7 +24,7 @@ for country in countries:
 
 change=pd.concat(subplot, ignore_index=True)
 
-fig,axes = plt.subplots(2,1,figsize=(10,8))
+fig,axes = plt.subplots(2,1,figsize=(10,9))
 
 for country in countries:
     data_country = change[change["Country Name"]==country]
@@ -32,9 +32,9 @@ for country in countries:
                  marker="o",linewidth=1.5,color="tab:red")
 
 axes[0].axhline(0, color ="black", linewidth=1)
-axes[0].set_ylabel("% change")
-axes[0].set_xlabel("Year")
-axes[0].set_title("Annual % change of the world in CO2 emissions per capita since 1980")
+axes[0].set_ylabel("% change",fontsize=11)
+axes[0].set_xlabel("Year",fontsize=11)
+axes[0].set_title("Annual % change of the world in CO2 emissions per capita since 1980",fontsize=11)
 axes[0].grid(True, alpha=0.3)
 
 for country in countries:
@@ -43,9 +43,9 @@ for country in countries:
                  marker="o",linewidth=1.5,color="tab:red")
 
 axes[1].axhline(0,color="black",linewidth=1)
-axes[1].set_xlabel("Year")
-axes[1].set_ylabel("% change")
-axes[1].set_title("Annual % change of the world in GDP per capita since 1980")
+axes[1].set_xlabel("Year",fontsize=11)
+axes[1].set_ylabel("% change",fontsize=11)
+axes[1].set_title("Annual % change of the world in GDP per capita since 1980",fontsize=11)
 axes[1].grid(True,alpha=0.3)
 
 for ax in axes:
